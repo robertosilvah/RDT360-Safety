df08215ae9619c49a99eaed2a668c4536856c9d3
RDT INC <rdtincusa@gmail.com>
2025-06-30 16:58:32 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyA58WZHhyhX-JZ2UlFdIfsiR6_rMP1vRfU",
+  authDomain: "rdt360-safety.firebaseapp.com",
+  projectId: "rdt360-safety",
+  storageBucket: "rdt360-safety.appspot.com",
+  messagingSenderId: "949644180482",
+  appId: "1:949644180482:web:58393615e49c106502fb1b"
 };
 
 // Initialize Firebase
