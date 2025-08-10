64307b62c4bb9b4dbed451aadff7f7406f719e08
RDT INC <rdtincusa@gmail.com>
2025-08-10 02:45:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -395,13 +395,3 @@ export default function HelpPage() {
     </AppShell>
   );
 }
-
-const CodeBlock = ({ code }: { code: string }) => {
-  return (
-    <div className="bg-gray-900 text-white rounded-md">
-      <div className="p-4 overflow-x-auto">
-        <pre><code className="language-sql">{code}</code></pre>
-      </div>
-    </div>
-  );
-};
